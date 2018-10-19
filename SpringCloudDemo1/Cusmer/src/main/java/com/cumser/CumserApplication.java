473f386ad7e637671e25ea30684c2c509ba2fd4a
leisure99 <578964701@qq.com>
2018-10-19 11:30:28 +0800

    第一次提交

---
+++ SpringCloudDemo1/Cusmer/src/main/java/com/cumser/CumserApplication.java
@@ -0,0 +1,23 @@
+package com.cumser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class CumserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CumserApplication.class,args);
+    }
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
